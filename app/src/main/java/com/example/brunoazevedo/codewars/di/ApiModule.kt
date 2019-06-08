17a5e16014a27d51bbc5bb88758de38f266f183a
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-08 19:13:58 +0100

    Create Dagger ApiModule to be able to inject the api

---
+++ app/src/main/java/com/example/brunoazevedo/codewars/di/ApiModule.kt
@@ -0,0 +1,24 @@
+package com.example.brunoazevedo.codewars.di
+
+import com.example.brunoazevedo.codewars.model.api.CodewarsAPI
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class ApiModule {
+
+    private val BASE_URL = "https://www.codewars.com/api/v1/"
+
+    @Provides
+    fun provideCodewarsAPI() : CodewarsAPI {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(CodewarsAPI::class.java)
+    }
+}
