5607be9363391f64a2f916951e08553fa5a1e43b
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-08 19:30:31 +0100

    Update ApiComponent to be possible to use dagger on Repository

--- app/src/main/java/com/example/brunoazevedo/codewars/di/ApiComponent.kt
+++ app/src/main/java/com/example/brunoazevedo/codewars/di/ApiComponent.kt
@@ -1,10 +1,13 @@
 package com.example.brunoazevedo.codewars.di
 
 import com.example.brunoazevedo.codewars.model.api.CodewarsService
+import com.example.brunoazevedo.codewars.model.repository.Repository
 import dagger.Component
 
 @Component(modules = [ApiModule::class])
 interface ApiComponent {
 
     fun inject(service : CodewarsService)
+
+    fun inject(repository : Repository)
 }
