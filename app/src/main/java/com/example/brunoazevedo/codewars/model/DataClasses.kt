2cf50e60d8225fda826e23f27f73ab9159da286e
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-08 18:44:10 +0100

    User model classes creation

---
+++ app/src/main/java/com/example/brunoazevedo/codewars/model/DataClasses.kt
@@ -0,0 +1,7 @@
+package com.example.brunoazevedo.codewars.model
+
+data class User(val name : String, val username : String, val ranks : Ranks )
+
+data class Ranks(val overall : Overall, val languages : Map<String, Overall>)
+
+data class Overall(val rank : Long, val name : String, val color : String, val score : Long)
