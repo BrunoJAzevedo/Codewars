2c2e9ef4363457700024fe8ed9cc89a527d1a546
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-09 19:30:50 +0100

    Request Internet Permission

--- app/src/main/java/com/example/brunoazevedo/codewars/di/AppComponent.kt
+++ app/src/main/java/com/example/brunoazevedo/codewars/di/AppComponent.kt
@@ -1,5 +1,6 @@
 package com.example.brunoazevedo.codewars.di
 
+import android.arch.lifecycle.ViewModel
 import com.example.brunoazevedo.codewars.model.api.CodewarsService
 import com.example.brunoazevedo.codewars.model.repository.Repository
 import dagger.Component
@@ -10,4 +11,6 @@ interface ApiComponent {
     fun inject(service : CodewarsService)
 
     fun inject(repository : Repository)
+
+    fun inject(viewModel: ViewModel)
 }
