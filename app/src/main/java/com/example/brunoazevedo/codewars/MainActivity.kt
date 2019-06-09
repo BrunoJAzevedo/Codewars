673917865b3c235a4be0b6d46d5d6a1914839ee5
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-09 19:46:28 +0100

    Change MainActivity to display UserFragment

--- app/src/main/java/com/example/brunoazevedo/codewars/MainActivity.kt
+++ app/src/main/java/com/example/brunoazevedo/codewars/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.brunoazevedo.codewars
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.brunoazevedo.codewars.views.UsersFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager?.beginTransaction()?.replace(R.id.fragment_container, UsersFragment.newInstance("users"), "users")?.addToBackStack(null)?.commit()
     }
 }
