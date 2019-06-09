cd555b155001287e4cf563c8d2edbff41a7a61ff
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-09 19:39:46 +0100

    Change ApiComponent to AppComponent inject Repository and UserViewModel

--- app/src/main/java/com/example/brunoazevedo/codewars/di/AppComponent.kt
+++ app/src/main/java/com/example/brunoazevedo/codewars/di/AppComponent.kt
@@ -1,16 +1,21 @@
 package com.example.brunoazevedo.codewars.di
 
-import android.arch.lifecycle.ViewModel
+import android.support.v4.app.Fragment
 import com.example.brunoazevedo.codewars.model.api.CodewarsService
 import com.example.brunoazevedo.codewars.model.repository.Repository
+import com.example.brunoazevedo.codewars.viewmodel.UserViewModel
 import dagger.Component
+import javax.inject.Singleton
 
-@Component(modules = [ApiModule::class])
-interface ApiComponent {
+@Component(modules = [
+    ApiModule::class
+])
+@Singleton
+interface AppComponent {
 
+    fun inject(repo : Repository)
     fun inject(service : CodewarsService)
+    fun inject(fragment : Fragment)
+    fun inject(viewModel : UserViewModel)
 
-    fun inject(repository : Repository)
-
-    fun inject(viewModel: ViewModel)
 }
