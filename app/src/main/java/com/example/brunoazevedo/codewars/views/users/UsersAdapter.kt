3fbf1d032f84255b6bfc8c082cf08d4da1215632
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-10 11:51:19 +0100

    UsersAdapter creation

---
+++ app/src/main/java/com/example/brunoazevedo/codewars/views/users/UsersAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.brunoazevedo.codewars.views.users
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import com.example.brunoazevedo.codewars.R
+import com.example.brunoazevedo.codewars.model.User
+import kotlinx.android.synthetic.main.item_user.view.*
+
+class UsersAdapter(private var users : ArrayList<User>) :
+    RecyclerView.Adapter<UsersAdapter.UsersViewHolder>()  {
+
+    fun updateUsers(newUsers : List<User>) {
+        users.clear()
+        users.addAll(newUsers)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = users.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int) = UsersViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent,false)
+    )
+
+    override fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
+        holder.bind(users[position])
+    }
+
+    class UsersViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+        private val name = view.Name
+        private val rank = view.Rank
+        private val laguage = view.Language
+        private val points = view.Points
+
+        fun bind(user : User) {
+            name.text = user.username
+            rank.text = user.ranks.overall.rank.toString()
+            laguage.text = "Language"
+            points.text = "Points"
+        }
+    }
+}
