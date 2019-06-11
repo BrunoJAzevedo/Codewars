cec3395bfc394b9a026c3910824104e31877fcc3
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-11 21:41:25 +0100

    Create Completed Challenges and Authored Challenges data class

--- app/src/main/java/com/example/brunoazevedo/codewars/model/DataClasses.kt
+++ app/src/main/java/com/example/brunoazevedo/codewars/model/DataClasses.kt
@@ -1,7 +1,35 @@
 package com.example.brunoazevedo.codewars.model
 
-data class User(val name : String, val username : String,  val leaderboardPosition : Long, val ranks : Ranks )
+import com.google.gson.annotations.SerializedName
+
+
+/*
+User
+ */
+data class User(val name : String, val username : String,  val leaderboardPosition : Long,
+                val ranks : Ranks )
 
 data class Ranks(val overall : Overall, val languages : Map<String, Overall>)
 
 data class Overall(val rank : Long, val name : String, val color : String, val score : Long)
+
+/*
+Completed Challenge
+ */
+data class CompletedChallenge(val totalPages : Long, val totalItems : Long,
+                              val data : List<CompletedChallengeData>)
+
+data class CompletedChallengeData(val id : String, val name : String, val slug : String,
+                                  @SerializedName("completedLanguages")val languages : List<String>,
+                                  val completedAt : String)
+
+/*
+Authored Challenge
+ */
+
+data class AuthoredChallenge(val data : List<AuthoredChallengeData>)
+
+data class AuthoredChallengeData(val id : String, val name : String, val description : String,
+                                 val ranks : Long, val rankName : String, val tags : List<String>,
+                                 val languages : List<String>)
+
