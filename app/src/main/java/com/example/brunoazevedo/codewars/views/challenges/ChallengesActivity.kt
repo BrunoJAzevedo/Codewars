3f651cf710d5802e531b11dad0ed6d455433daab
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-12 11:52:56 +0100

    Challenges will not be a Fragment but an activity

--- app/src/main/java/com/example/brunoazevedo/codewars/views/challenges/ChallengesActivity.kt
+++ app/src/main/java/com/example/brunoazevedo/codewars/views/challenges/ChallengesActivity.kt
@@ -2,46 +2,29 @@ package com.example.brunoazevedo.codewars.views.challenges
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.brunoazevedo.codewars.R
 import com.example.brunoazevedo.codewars.utils.userString
 import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.MenuItem
 import com.example.brunoazevedo.codewars.utils.challengeString
 import com.example.brunoazevedo.codewars.views.challenges.completed.ChallengesCompletedFragment
-import kotlinx.android.synthetic.main.challenges_fragment.*
+import kotlinx.android.synthetic.main.activity_challenge.*
 
 
-class ChallengesFragment : Fragment() {
-
-    companion object {
-        private const val KEY: String = ""
-
-        @JvmStatic
-        fun newInstance(url: String) =
-            ChallengesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(KEY, url)
-                }
-            }
-    }
+class ChallengesActivity : AppCompatActivity() {
 
     private var _username : String? = ""
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.challenges_fragment, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge)
 
-        _username = arguments?.getString(userString)
-
-        return rootView
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        _username = intent.getStringExtra(userString)
         bottom_navigation.setOnNavigationItemSelectedListener(_OnNavigationItemSelectedListener)
         bottom_navigation.selectedItemId = R.id.challenges_completed
+
     }
 
     private val _OnNavigationItemSelectedListener = object : BottomNavigationView.OnNavigationItemSelectedListener {
@@ -67,10 +50,8 @@ class ChallengesFragment : Fragment() {
         args.putString(userString, _username)
         completedChallengesFragment.arguments = args
 
-        val transaction = activity?.supportFragmentManager?.beginTransaction()
-        transaction?.replace(R.id.challenges_fragment_container, completedChallengesFragment,
-            "completedChallengeString"
-        )?.addToBackStack(null)?.commit()
+        supportFragmentManager?.beginTransaction()?.replace(R.id.challenges_fragment_container, completedChallengesFragment,
+            challengeString)?.addToBackStack(null)?.commit()
     }
 
     private fun displayAuthoredChallengesFragment() {
