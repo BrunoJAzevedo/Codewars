4f41cb4c0c8b1fdd3c34c7ee92a4e789417d791c
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-06-12 21:26:43 +0100

    Info Challenges data classes creation

--- app/src/main/java/com/example/brunoazevedo/codewars/model/DataClasses.kt
+++ app/src/main/java/com/example/brunoazevedo/codewars/model/DataClasses.kt
@@ -34,3 +34,20 @@ data class AuthoredChallengeData(val id : String, val name : String, val descrip
                                  val ranks : Long, val rankName : String, val tags : List<String>,
                                  val languages : List<String>)
 
+/*
+Info Challenge
+ */
+data class InfoChallenge(val id : String, val name : String, val category : String, val publishedAt : String,
+                         val approvedAt : String, val languages : List<String>, val url : String,
+                         val rank : Rank, val createdAt : String, val createdBy : UserInfo,
+                         val approvedBy : UserInfo, val description : String, val totalAttempts : Int,
+                         val totalCompleted : Int, val totalStars : Int, val voteScore : Int,
+                         val tags : List<String>,
+                         @SerializedName("contributorsWanted")val helpNeeded : Boolean,
+                         val unresolved : Unresolved)
+
+data class Rank(val id : Int, val name : String, val color : String)
+
+data class UserInfo(val username : String, val url : String)
+
+data class Unresolved(val issues : Int, val suggestions : Int)
